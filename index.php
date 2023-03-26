79194bf3b9dacc67c48969b765114020c8249ef1
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-03-26 15:11:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ if (empty($_POST['amount_of_limbs']) || !is_numeric($_POST['amount_of_limbs']) |
   $errors = TRUE;
 }
 
-if (empty($_POST['abilities'])) || if 
+if (empty($_POST['abilities']) OR 
 (foreach ($_POST['abilities'] as $ability) {!($ability=='Бессмертие' || $ability=='Прохождение сквозь стены' || $ability=='Левитация')}) {
 //if (empty($_POST['abilities']) || !($_POST['abilities']=='Бессмертие' || $_POST['abilities']=='Прохождение сквозь стены' || $_POST['abilities']=='Левитация')) {
   print('Выберите сверхспособности.<br/>');
