5ebcab3e7be6ba7b0f99c535e21ca0eccd27aa21
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-03-26 17:08:40 +0300

    Update form.php

--- form.php
+++ form.php
@@ -1,32 +1,66 @@
 <head>
         <link rel="stylesheet" href="style.css">
 </head> 
+<body> 
+        
+<?php
+if (!empty($messages)) {
+  print('<div id="messages">');
+  // Выводим все сообщения.
+  foreach ($messages as $message) {
+    print($message);
+  }
+  print('</div>');
+}
+?>
+
 <form action="" class="forma" method="POST">
     <label>
         Имя:<br>
-        <input id="data" name="name" placeholder="Введите Ваше имя">
+        <input id="data" name="name" placeholder="Введите Ваше имя" <?php if ($errors['name']) {print 'class="error"';} ?> value="<?php print $values['name']; ?>" >
     </label><br>
     <label>
         Email:<br>
-        <input id="data" name="email" type="email" placeholder="Введите вашу почту">
+        <input id="data" name="email" type="email" placeholder="Введите вашу почту" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>" >
     </label><br>
     <label>
         Дата рождения:<br>
-     
-     <!--<input id="data" name="birth_date" type="date">-->
-        
-         <select id="data" name="birth_date">
-         <?php 
-          for ($i = 1922; $i <= 2022; $i++) {
-          printf('<option value="%d">%d год</option>', $i, $i);
-          }
+         <select id="data" name="birth_date" <?php if ($errors['birth_date']) {print 'class="error"';} ?> >
+         <?php  
+                 
+                 if ($values['birth_date']=='') {
+                         for ($i = 1922; $i <= 2022; $i++) {
+                            printf('<option value="%d">%d год</option>', $i, $i);
+                         }
+                 }
+                 else {
+                         print('<option value="<?php print $values['birth_date']; ?>" />');
+                         }
          ?>
         </select>
-        
     </label><br>
+          
     Пол:<br>
-      <label><input id="data" type="radio" name="sex" value="ж">Ж</label>
-      <label><input id="data" type="radio" name="sex" value="м">М</label><br />
+        <div <?php if ($errors['sex']) {print 'class="error"';} ?> >
+        <?php
+                if ($values['sex']=='') {
+                           print ('<label><input id="data" type="radio" name="sex" value="ж">Ж</label>');
+                           print ('<label><input id="data" type="radio" name="sex" value="м">М</label><br />');    
+                }
+                else {
+                        if ($values['sex']=='м') {
+                                 print ('<label><input id="data" type="radio" name="sex" value="ж">Ж</label>');
+                                print ('<label><input id="data" type="radio" name="sex" value="м" checked="checked">М</label><br />');
+                        }
+                        else {
+                                print ('<label><input id="data" type="radio" name="sex" value="ж" checked="checked">Ж</label><br />');
+                                print ('<label><input id="data" type="radio" name="sex" value="м">М</label><br />');
+                        }
+                }
+     
+        ?>
+        </div>
+        
     Количество конечностей:<br />
       <label><input id="data" type="radio" name="amount_of_limbs" value="2"> 2 </label>
       <label><input id="data" type="radio" name="amount_of_limbs" value="3"> 3 </label>
@@ -46,3 +80,4 @@
     <label><input id="data" type="checkbox" name="informed">С контрактом ознакомлен(а)</label><br>
     <input id="sub" type="submit" value="Отправить">
   </form>
+</body>
